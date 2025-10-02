9e7b1767ee90a9fe6491d151497e90aa6eacf681
LucasKim96 <kimanhnguyen8723@gmail.com>
2025-10-02 16:32:16 +0700

    connect db fix

--- server/src/main.js
+++ server/src/main.js
@@ -3,15 +3,15 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import productRoutes from "./modules/product/product.route.js";
-import authRoutes from "./modules/auth/auth.route.js";
-import searchRoutes from "./modules/search/search.route.js";
-import userRoutes from "./modules/user/user.route.js";
-import {
-  authMiddleware,
-  errorMiddleware,
-  loggerMiddleware,
-} from "./middlewares/index.js";
+// import productRoutes from "./modules/product/product.route.js";
+// import authRoutes from "./modules/auth/auth.route.js";
+// import searchRoutes from "./modules/search/search.route.js";
+// import userRoutes from "./modules/user/user.route.js";
+// import {
+//   authMiddleware,
+//   errorMiddleware,
+//   loggerMiddleware,
+// } from "./middlewares/index.js";
 
 dotenv.config();
 
@@ -23,10 +23,10 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/api/products", productRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/search", searchRoutes);
-app.use("/api/users", userRoutes);
+// app.use("/api/products", productRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/search", searchRoutes);
+// app.use("/api/users", userRoutes);
 
 // Connect MongoDB and start server
 connectDB(process.env.MONGO_URI);
