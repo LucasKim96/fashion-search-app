a7a50b360aec475fb61159d951a92e045d74aa96
Cam Truc <truc.0965000900@gmail.com>
2025-10-11 12:48:55 +0700

    feat: add authentication api

--- server/src/modules/user/userInfo.model.js
+++ server/src/modules/user/userInfo.model.js
@@ -1,7 +1,9 @@
+import mongoose from "mongoose";
+
 const userInfoSchema = new mongoose.Schema({
   name: { type: String, default: "", trim: true },
   dayOfBirth: { type: Date, default: null },
-  avatar: { type: String, default: "" }, // có thể để URL mặc định
+  avatar: { type: String, default: "/assets/default-avatar.jpg" }, // có thể để URL mặc định
   email: { type: String, unique: true, sparse: true },
   gender: { type: String, enum: ["male", "female", "other"], default: "other" },
 }, { timestamps: true });
