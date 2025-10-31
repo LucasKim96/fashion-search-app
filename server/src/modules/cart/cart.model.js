28c740470ce436b70d1d256887c8a48c312e56cd
LucasKim96 <kimanhnguyen8723@gmail.com>
2025-10-31 21:01:43 +0700

    sv

--- server/src/modules/cart/cart.model.js
+++ server/src/modules/cart/cart.model.js
@@ -1,17 +1,44 @@
-// Cart Model
 import mongoose from "mongoose";
 
-const cartSchema = new mongoose.Schema({
-  accountId: { type: mongoose.Schema.Types.ObjectId, ref: "Account" },
-  cartItems: [
+const cartItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  productVariantId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "ProductVariant",
+    required: true,
+  },
+  quantity: { type: Number, required: true, min: 1 },
+
+  // Snapshot dữ liệu lúc thêm (phòng khi Product/Variant bị xóa hoặc đổi)
+  priceAtAdd: { type: Number, required: true },
+  imageAtAdd: { type: String },
+  pdNameAtAdd: { type: String },
+
+  // Các thuộc tính của variant (vd: Color: Red, Size: M)
+  attributes: [
     {
-      productVariantId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "ProductVariant",
-      },
-      quantity: Number,
+      attributeId: { type: mongoose.Schema.Types.ObjectId, ref: "Attribute" },
+      valueId: { type: mongoose.Schema.Types.ObjectId, ref: "AttributeValue" },
     },
   ],
 });
+
+const cartSchema = new mongoose.Schema(
+  {
+    accountId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Account",
+      required: true,
+    },
+    cartItems: [cartItemSchema],
+  },
+  { timestamps: true }
+);
+
+cartSchema.index({ accountId: 1 }); // dễ truy vấn theo người dùng
 
 export default mongoose.model("Cart", cartSchema);
--- server/src/modules/index.js
+++ server/src/modules/index.js
@@ -1,10 +1,14 @@
 // server/src/modules/index.js
 import "./shop/shop.model.js";
 import "./cart/cart.model.js";
-import "./product/product.model.js";
 import "./account/account.model.js";
 import "./order/order.model.js";
 import "./user/userInfo.model.js";
+import "./product/product.model.js";
+import "./product/productVariant.model";
+import "./product/attribute.model.js";
+import "./product/attributeValue.model.js";
+import "./product/shopAttributeValue.model.js";
 
 export * from "./account/index.js";
 export * from "./auth/index.js";
