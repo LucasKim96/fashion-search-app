e77a6a25213229b4c4b02d0811d39d2113ab415e
Cam Truc <truc.0965000900@gmail.com>
2025-11-09 00:04:39 +0700

    save fe login

--- shared/features/auth/LoginPage.tsx
+++ shared/features/auth/LoginPage.tsx
@@ -1,13 +1,29 @@
-import { LoginForm } from "./LoginForm.component";
+"use client";
+import { LoginForm } from "./LoginForm.component"; // Đã thêm .tsx
+import  styles  from "./LoginCustom.module.css";
+interface LoginPageProps {
+  title?: string;
+  redirectPath?: string;
+}
 
 export const LoginPage = ({
   title,
   redirectPath = "/dashboard",
-}: {
-  title?: string;
-  redirectPath?: string;
-}) => (
-  <div className="flex items-center justify-center min-h-screen bg-gray-900">
-    <LoginForm title={title} redirectPath={redirectPath} />
-  </div>
-);
+}: LoginPageProps) => {
+  return (
+    // Sử dụng .auth-wrapper để áp dụng full screen, background và center
+    <div className={styles["auth-wrapper"]}> 
+
+      {/* Hero Title: Sẽ sử dụng CSS của .hero-title */}
+      <div className={styles["hero-title"]}>
+        <h1>Fashion</h1>
+        <h3>Fashion search</h3>
+      </div>
+
+      {/* Auth Form: Sẽ sử dụng CSS của .auth-form */}
+      <div className={styles["auth-form"]}>
+        <LoginForm title={title} redirectPath={redirectPath} />
+      </div>
+    </div>
+  );
+};
