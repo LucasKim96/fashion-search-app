c1fa42b91bc8cb90021115de8ac501c644bd0020
KimAnh <kimanhnguyen8723@gmail.com>
2025-11-13 23:28:22 +0700

    sv

--- model_api/app/config.py
+++ model_api/app/config.py
@@ -1,7 +1,10 @@
 import os
 
+PORT = int(os.getenv("PORT", 8000))
+HOST = os.getenv("HOST", "0.0.0.0")
+
 # model paths
-TEXT2IMG_MODEL_PATH = os.getenv("TEXT2IMG_MODEL_PATH", "models/text2img_best_epoch100.pt")
+TEXT2IMG_MODEL_PATH = os.getenv("TEXT2IMG_MODEL_PATH", "models/text2img_best_epoch101.pt")
 IMG2IMG_MODEL_PATH = os.getenv("IMG2IMG_MODEL_PATH", "models/image2img_best_epoch.pt")
 
 # index paths
